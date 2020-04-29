d6b9b03d3628316685ae5190c751db354859fe29
Gwonsoo Lee <gslee@bighitcorp.com>
2020-04-29 19:32:52 +0900

    add kms code

--- terraform/variables/var_route53.tf
+++ terraform/variables/var_route53.tf
@@ -1,10 +1,10 @@
 variable "r53_variables" {
   default = {
     prod = {
-      dayonedevops_com_zone_id = ""
+      dayonedevops_com_zone_id = "Z07279892AEB4RAPQCW8M"
 
-      star_dayonedevops_com_acm_arn_apnortheast2 = ""
-      star_dayonedevops_com_acm_arn_useast1      = ""
+      star_dayonedevops_com_acm_arn_apnortheast2 = "arn:aws:acm:ap-northeast-2:229438263680:certificate/89b65af1-d1b2-4130-b8ca-64f344da5f2a"
+      star_dayonedevops_com_acm_arn_useast1      = "arn:aws:acm:us-east-1:229438263680:certificate/04de536d-0fac-4ba7-b706-574902343475"
       www_dayonedevops_com_acm_arn_useast1       = ""
     }
   }
