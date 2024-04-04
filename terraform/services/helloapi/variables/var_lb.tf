1482f12c4edaf1a5013c5109108ea2837d3e92ec
jupitersong <jupitersong@lguplus.co.kr>
2024-04-04 23:16:28 +0900

    201-final create internal NLB, trouble shoot, import.

--- terraform/services/helloapi/variables/var_lb.tf
+++ terraform/services/helloapi/variables/var_lb.tf
@@ -32,23 +32,10 @@ variable "lb_variables" {
           project = "helloapi"
           env     = "dev"
           stack   = "devarts_apnortheast2"
-        },
-
-        artdapne2 = {
-          Name    = "helloapi-artd_apnortheast2-external-lb"
-          app     = "helloapi"
-          project = "helloapi"
-          env     = "dev"
-          stack   = "artd_apnortheast2"
-        },
-
-        artpapne2 = {
-          Name    = "helloapi-artp_apnortheast2-external-lb"
-          app     = "helloapi"
-          project = "helloapi"
-          env     = "prod"
-          stack   = "artp_apnortheast2"
         }
+
+
+
       }
     }
 
@@ -88,5 +75,66 @@ variable "lb_variables" {
         }
       }
     }
+    internal_lb = {
+      tags = {
+
+        devartdapne2 = {
+          Name    = "helloapi-devartd_apnortheast2-internal-lb"
+          app     = "helloapi"
+          project = "helloapi"
+          env     = "dev"
+          stack   = "devartd_apnortheast2"
+        },
+
+        devartsapne2 = {
+          Name    = "helloapi-devarts_apnortheast2-internal-lb"
+          app     = "helloapi"
+          project = "helloapi"
+          env     = "dev"
+          stack   = "devarts_apnortheast2"
+        }
+
+
+
+      }
+    }
+
+    internal_lb_tg = {
+      tags = {
+
+        devartdapne2 = {
+          Name    = "helloapi-devartd_apnortheast2-internal-tg"
+          app     = "helloapi"
+          project = "helloapi"
+          env     = "dev"
+          stack   = "devartd_apnortheast2"
+        },
+
+        devartsapne2 = {
+          Name    = "helloapi-devarts_apnortheast2-internal-tg"
+          app     = "helloapi"
+          project = "helloapi"
+          env     = "dev"
+          stack   = "devarts_apnortheast2"
+        },
+
+        artdapne2 = {
+          Name    = "helloapi-artd_apnortheast2-internal-tg"
+          app     = "helloapi"
+          project = "helloapi"
+          env     = "dev"
+          stack   = "artd_apnortheast2"
+        },
+
+        artpapne2 = {
+          Name    = "helloapi-artp_apnortheast2-internal-tg"
+          app     = "helloapi"
+          project = "helloapi"
+          env     = "prod"
+          stack   = "artp_apnortheast2"
+        }
+      }
+    }
+
   }
 }
